22b7cb00a14ba32a2fa9eda658af0b14a757255d
Giles <1305088289@qq.com>
2023-03-23 14:25:37 +0800

    update log

--- Log/log.cpp
+++ Log/log.cpp
@@ -1,55 +1,51 @@
 #include "./log.hpp"
 
+#include <assert.h>  //assert
 #include <errno.h>
-#include <unistd.h>//access, getpid
-#include <assert.h>//assert
-#include <stdarg.h>//va_list
-#include <sys/stat.h>//mkdir
-#include <sys/syscall.h>//system call
+#include <stdarg.h>       //va_list
+#include <sys/stat.h>     //mkdir
+#include <sys/syscall.h>  //system call
+#include <unistd.h>       //access, getpid
 
-#define MEM_USE_LIMIT (3u * 1024 * 1024 * 1024)//3GB
-#define LOG_USE_LIMIT (1u * 1024 * 1024 * 1024)//1GB
-#define LOG_LEN_LIMIT (4 * 1024)//4K
+#define MEM_USE_LIMIT (3u * 1024 * 1024 * 1024)  // 3GB, 所有日志加起来最多MEM_USE_LIMIT字节
+#define LOG_USE_LIMIT (1u * 1024 * 1024 * 1024)  // 1GB, 一个日志文件最多LOG_USE_LIMIT字节
+#define LOG_LEN_LIMIT (4 * 1024)                 // 4K, 一行日志最多LOG_LEN_LIMIT字节
 #define RELOG_THRESOLD 5
 #define BUFF_WAIT_TIME 1
 
-/* pid_t gettid() {
+pid_t gettid() {
     return syscall(__NR_gettid);
-} */
+}
 
 pthread_mutex_t ring_log::_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t ring_log::_cond = PTHREAD_COND_INITIALIZER;
 
 ring_log* ring_log::_ins = NULL;
 pthread_once_t ring_log::_once = PTHREAD_ONCE_INIT;
-uint32_t ring_log::_one_buff_len = 30*1024*1024;//30MB
+uint32_t ring_log::_one_buff_len = 30 * 1024 * 1024;  // 30MB
 
 /* 初始化，以及建立双向链表 */
-ring_log::ring_log():
-    _buff_cnt(3),
-    _curr_buf(NULL),
-    _prst_buf(NULL),
-    _fp(NULL),
-    _log_cnt(0),
-    _env_ok(false),
-    _level(INFO),
-    _lst_lts(0),
-    _tm()
-{
-    //create double linked list
+ring_log::ring_log()
+    : _buff_cnt(3),
+      _curr_buf(NULL),
+      _prst_buf(NULL),
+      _fp(NULL),
+      _log_cnt(0),
+      _env_ok(false),
+      _level(INFO),
+      _lst_lts(0),
+      _tm() {
+    /* 创建双向循环链表 */
     cell_buffer* head = new cell_buffer(_one_buff_len);
-    if (!head)
-    {
+    if (!head) {
         fprintf(stderr, "no space to allocate cell_buffer\n");
         exit(1);
     }
     cell_buffer* current;
     cell_buffer* prev = head;
-    for (int i = 1;i < _buff_cnt; ++i)
-    {
+    for (int i = 1; i < _buff_cnt; ++i) {
         current = new cell_buffer(_one_buff_len);
-        if (!current)
-        {
+        if (!current) {
             fprintf(stderr, "no space to allocate cell_buffer\n");
             exit(1);
         }
@@ -63,27 +59,31 @@ ring_log::ring_log():
     _curr_buf = head;
     _prst_buf = head;
 
+    /* cell buffer双线链表创建完毕之后，获取当前进程的id */
     _pid = getpid();
 }
 
-void ring_log::init_path(const char* log_dir, const char* prog_name, int level)
-{
+void ring_log::init_path(const char* log_dir,
+                         const char* prog_name,
+                         int level) {
     pthread_mutex_lock(&_mutex);
 
+    /* 拷贝 log 文件夹路径和程序名 */
     strncpy(_log_dir, log_dir, 512);
-    //name format:  name_year-mon-day-t[tid].log.n
+    /* _prog_name的格式:  name_year-mon-day-t[tid].log.n */
     strncpy(_prog_name, prog_name, 128);
 
+    /* 创建存储的log文件的文件夹 */
     mkdir(_log_dir, 0777);
-    //查看是否存在此目录、目录下是否允许创建文件
-    if (access(_log_dir, F_OK | W_OK) == -1)
-    {
+
+    /* 查看是否存在此目录、目录下是否允许创建文件 */
+    if (access(_log_dir, F_OK | W_OK) == -1) {
         fprintf(stderr, "logdir: %s error: %s\n", _log_dir, strerror(errno));
-    }
-    else
-    {
+    } else {
         _env_ok = true;
     }
+
+    /* 检查并设置记录的级别，如果超出了允许的范围，则设为最高或最低级别 */
     if (level > TRACE)
         level = TRACE;
     if (level < FATAL)
@@ -93,43 +93,49 @@ void ring_log::init_path(const char* log_dir, const char* prog_name, int level)
     pthread_mutex_unlock(&_mutex);
 }
 
-void ring_log::persist()
-{
-    while (true)
-    {
-        //check if _prst_buf need to be persist
+/**
+ * @brief 持久化，从文件中向缓冲区中追加内容到文件(磁盘)，该函数是给消费者使用的。
+ * 信号量超时时间为1s，超过一秒即便该缓冲区仍然未满：消费者将此cell_buffer标记为FULL强行持久化
+ */
+void ring_log::persist() {
+    while (true) {
+        /* 检查 _prst_buf 是否需要持久化 */
         pthread_mutex_lock(&_mutex);
-        if (_prst_buf->status == cell_buffer::FREE)
-        {
+        if (_prst_buf->status == cell_buffer::FREE) {
+            /* 如果持久化缓存仍然可追加日志，那就阻塞当前线程 */
             struct timespec tsp;
             struct timeval now;
             gettimeofday(&now, NULL);
             tsp.tv_sec = now.tv_sec;
-            tsp.tv_nsec = now.tv_usec * 1000;//nanoseconds
-            tsp.tv_sec += BUFF_WAIT_TIME;//wait for 1 seconds
+            tsp.tv_nsec = now.tv_usec * 1000;  
+            tsp.tv_sec += BUFF_WAIT_TIME;       /* 设置超时事件为1s */
+            /* 超过一秒即便该缓冲区仍然未满：也要解除阻塞 */
             pthread_cond_timedwait(&_cond, &_mutex, &tsp);
         }
-        if (_prst_buf->empty())
-        {
-            //give up, go to next turn
+
+        if (_prst_buf->empty()) {
+            /* 如果持久化内存为空，则放弃，进入下一个循环 */
             pthread_mutex_unlock(&_mutex);
             continue;
         }
 
-        if (_prst_buf->status == cell_buffer::FREE)
-        {
-            assert(_curr_buf == _prst_buf);//to test
+        if (_prst_buf->status == cell_buffer::FREE) {
+            /* 接触信号量阻塞之后再次检测，如果缓存依旧可追加，说明当前生产者还在该buffer */
+            assert(_curr_buf == _prst_buf); 
+            /* 即便生产者仍然在向buffer写入，但是消费者也要强制持久化了 */
             _curr_buf->status = cell_buffer::FULL;
+            /* 让生产者去下一个buffer生产 */
             _curr_buf = _curr_buf->next;
         }
 
         int year = _tm.year, mon = _tm.mon, day = _tm.day;
         pthread_mutex_unlock(&_mutex);
 
-        //decision which file to write
+        // decision which file to write
         if (!decis_file(year, mon, day))
             continue;
-        //write
+        
+        /* 持久化：将缓冲区内容写入到文件 */
         _prst_buf->persist(_fp);
         fflush(_fp);
 
@@ -140,22 +146,30 @@ void ring_log::persist()
     }
 }
 
-void ring_log::try_append(const char* lvl, const char* format, ...)
-{
+/**
+ * @brief 将一条日志加入到环形缓冲区中，如果当前缓冲区已满，则将日志写入下一个缓冲区
+ * @param lvl：日志等级
+ * @param format：日志格式
+ * @param ...：变长参数列表，对应于日志格式字符串中的占位符
+ */
+void ring_log::try_append(const char* lvl, const char* format, ...) {
     int ms;
     uint64_t curr_sec = _tm.get_curr_time(&ms);
+
+    /* 如果距离上一次日志写入的时间小于阈值 RELOG_THRESOLD，则不写入 */
     if (_lst_lts && curr_sec - _lst_lts < RELOG_THRESOLD)
-        return ;
+        return;
 
     char log_line[LOG_LEN_LIMIT];
-    //int prev_len = snprintf(log_line, LOG_LEN_LIMIT, "%s[%d-%02d-%02d %02d:%02d:%02d.%03d]", lvl, _tm.year, _tm.mon, _tm.day, _tm.hour, _tm.min, _tm.sec, ms);
+    /* 格式化时间戳，将等级和时间戳拼接到 log_line 中 */
     int prev_len = snprintf(log_line, LOG_LEN_LIMIT, "%s[%s.%03d]", lvl, _tm.utc_fmt, ms);
 
     va_list arg_ptr;
     va_start(arg_ptr, format);
 
-    //TO OPTIMIZE IN THE FUTURE: performance too low here!
-    int main_len = vsnprintf(log_line + prev_len, LOG_LEN_LIMIT - prev_len, format, arg_ptr);
+    /* 格式化变长参数列表中的日志信息，并将结果拼接到 log_line 的后面 */
+    int main_len = vsnprintf(log_line + prev_len, LOG_LEN_LIMIT - prev_len,
+                             format, arg_ptr);
 
     va_end(arg_ptr);
 
@@ -165,33 +179,28 @@ void ring_log::try_append(const char* lvl, const char* format, ...)
     bool tell_back = false;
 
     pthread_mutex_lock(&_mutex);
-    if (_curr_buf->status == cell_buffer::FREE && _curr_buf->avail_len() >= len)
-    {
+    /* 如果当前缓冲区未满且足够写入当前数据，将日志写入当前缓冲区 */
+    if (_curr_buf->status == cell_buffer::FREE && _curr_buf->avail_len() >= len) {
         _curr_buf->append(log_line, len);
-    }
-    else
-    {
-        //1. _curr_buf->status = cell_buffer::FREE but _curr_buf->avail_len() < len
-        //2. _curr_buf->status = cell_buffer::FULL
-        if (_curr_buf->status == cell_buffer::FREE)
-        {
-            _curr_buf->status = cell_buffer::FULL;//set to FULL
-            cell_buffer* next_buf = _curr_buf->next;
-            //tell backend thread
-             tell_back = true;
+    } else {
+        /* 当前缓冲区已满或者不够写入当前数据，需要将日志写入下一个缓冲区 */
 
-            //it suggest that this buffer is under the persist job
-            if (next_buf->status == cell_buffer::FULL)
-            {
-                //if mem use < MEM_USE_LIMIT, allocate new cell_buffer
-                if (_one_buff_len * (_buff_cnt + 1) > MEM_USE_LIMIT)
-                {
+        if (_curr_buf->status == cell_buffer::FREE) {
+            /* 如果当前缓冲区未满，但是不够写入len大小的数据了，要去下一个缓冲区 */
+            _curr_buf->status = cell_buffer::FULL;  // set to FULL
+            cell_buffer* next_buf = _curr_buf->next;
+            /* 告诉后端线程，当前缓冲区已满，需要将其写入磁盘 */
+            tell_back = true;
+
+            // 如果下一个缓冲区也是 FULL 状态，则无法使用下一个缓冲区，需要新开一个缓冲区
+            if (next_buf->status == cell_buffer::FULL) {
+                // 如果内存使用量已达到上限MEM_USE_LIMIT，则无法新开缓冲区，只能将当前缓冲区设置为下一个缓冲区
+                if (_one_buff_len * (_buff_cnt + 1) > MEM_USE_LIMIT) {
                     fprintf(stderr, "no more log space can use\n");
                     _curr_buf = next_buf;
                     _lst_lts = curr_sec;
-                }
-                else
-                {
+                } else {
+                    /* 创建一个新的缓存区，并加入双向链表 */
                     cell_buffer* new_buffer = new cell_buffer(_one_buff_len);
                     _buff_cnt += 1;
                     new_buffer->prev = _curr_buf;
@@ -200,71 +209,78 @@ void ring_log::try_append(const char* lvl, const char* format, ...)
                     next_buf->prev = new_buffer;
                     _curr_buf = new_buffer;
                 }
-            }
-            else
-            {
-                //next buffer is free, we can use it
+            } else {
+                /* 下一个缓冲区为free，可以直接进行写入 */
                 _curr_buf = next_buf;
             }
-            if (!_lst_lts)
+            if (!_lst_lts) {
+                /* 如果未达到MEM_USE_LIMIT，_lst_lts为0，可以直接追加日志 */
                 _curr_buf->append(log_line, len);
-        }
-        else//_curr_buf->status == cell_buffer::FULL, assert persist is on here too!
+            }
+                
+        } else  //_curr_buf->status == cell_buffer::FULL, assert persist is on here too!
         {
+            /* 当前缓冲区已满，无法追加日志 */
             _lst_lts = curr_sec;
         }
     }
     pthread_mutex_unlock(&_mutex);
-    if (tell_back)
-    {
+    if (tell_back) {
+        /* 让消费者线程解除阻塞，开启持久化工作 */
         pthread_cond_signal(&_cond);
     }
 }
 
-bool ring_log::decis_file(int year, int mon, int day)
-{
-    //TODO: 是根据日志消息的时间写时间？还是自主写时间？  I select 自主写时间
-    if (!_env_ok)
-    {
+/**
+ * @brief 决定根据日志消息的时间写时间？还是自主写时间？默认自主写时间
+ * @return 操作是否成功
+ */
+bool ring_log::decis_file(int year, int mon, int day) {
+    if (!_env_ok) {
+        /* 打开文件 */
         if (_fp)
             fclose(_fp);
         _fp = fopen("/dev/null", "w");
         return _fp != NULL;
     }
-    if (!_fp)
-    {
+
+    if (!_fp) {
+        /* 文件指针为空的话，创建一个文件并打开 */
         _year = year, _mon = mon, _day = day;
         char log_path[1024] = {};
-        sprintf(log_path, "%s/%s.%d%02d%02d.%u.log", _log_dir, _prog_name, _year, _mon, _day, _pid);
+        sprintf(log_path, "%s/%s.%d%02d%02d.%u.log", _log_dir, _prog_name,
+                _year, _mon, _day, _pid);
         _fp = fopen(log_path, "w");
         if (_fp)
             _log_cnt += 1;
-    }
-    else if (_day != day)
-    {
+    } else if (_day != day) {
+        /* 文件指针非空，先关闭再打开 */
         fclose(_fp);
         char log_path[1024] = {};
         _year = year, _mon = mon, _day = day;
-        sprintf(log_path, "%s/%s.%d%02d%02d.%u.log", _log_dir, _prog_name, _year, _mon, _day, _pid);
+        sprintf(log_path, "%s/%s.%d%02d%02d.%u.log", _log_dir, _prog_name,
+                _year, _mon, _day, _pid);
         _fp = fopen(log_path, "w");
         if (_fp)
             _log_cnt = 1;
-    }
-    else if (ftell(_fp) >= LOG_USE_LIMIT)
-    {
+    } else if (ftell(_fp) >= LOG_USE_LIMIT) {
+        /* _fp使用的字符长度已经超过的LOG_USE_LIMIT的话，重新开一个文件 */
         fclose(_fp);
         char old_path[1024] = {};
         char new_path[1024] = {};
-        //mv xxx.log.[i] xxx.log.[i + 1]
-        for (int i = _log_cnt - 1;i > 0; --i)
-        {
-            sprintf(old_path, "%s/%s.%d%02d%02d.%u.log.%d", _log_dir, _prog_name, _year, _mon, _day, _pid, i);
-            sprintf(new_path, "%s/%s.%d%02d%02d.%u.log.%d", _log_dir, _prog_name, _year, _mon, _day, _pid, i + 1);
+        /* 更新log文件的名字，即.log为最新文件， mv xxx.log.[i] xxx.log.[i + 1] */
+        for (int i = _log_cnt - 1; i > 0; --i) {
+            sprintf(old_path, "%s/%s.%d%02d%02d.%u.log.%d", _log_dir,
+                    _prog_name, _year, _mon, _day, _pid, i);
+            sprintf(new_path, "%s/%s.%d%02d%02d.%u.log.%d", _log_dir,
+                    _prog_name, _year, _mon, _day, _pid, i + 1);
             rename(old_path, new_path);
         }
-        //mv xxx.log xxx.log.1
-        sprintf(old_path, "%s/%s.%d%02d%02d.%u.log", _log_dir, _prog_name, _year, _mon, _day, _pid);
-        sprintf(new_path, "%s/%s.%d%02d%02d.%u.log.1", _log_dir, _prog_name, _year, _mon, _day, _pid);
+        // mv xxx.log xxx.log.1
+        sprintf(old_path, "%s/%s.%d%02d%02d.%u.log", _log_dir, _prog_name,
+                _year, _mon, _day, _pid);
+        sprintf(new_path, "%s/%s.%d%02d%02d.%u.log.1", _log_dir, _prog_name,
+                _year, _mon, _day, _pid);
         rename(old_path, new_path);
         _fp = fopen(old_path, "w");
         if (_fp)
@@ -273,8 +289,7 @@ bool ring_log::decis_file(int year, int mon, int day)
     return _fp != NULL;
 }
 
-void* be_thdo(void* args)
-{
+void* be_thdo(void* args) {
     ring_log::ins()->persist();
     return NULL;
 }
--- Log/log.hpp
+++ Log/log.hpp
@@ -1,5 +1,5 @@
-#ifndef LOG_H__
-#define LOG_H__
+#ifndef __RING_LOG_H__
+#define __RING_LOG_H__
 
 #include <stdio.h>
 #include <unistd.h>//access, getpid
@@ -12,8 +12,7 @@
 #include <sys/types.h>//getpid, gettid
 #include <sys/syscall.h>//system call
 
-#define Log ring_log
-
+/* 日志级别 */
 enum LOG_LEVEL
 {
     FATAL = 1,
@@ -24,19 +23,21 @@ enum LOG_LEVEL
     TRACE,
 };
 
-// extern pid_t gettid(void);
+// extern pid_t gettid();
 
+/* 时间类 */
 struct utc_timer
 {
     utc_timer()
     {
         struct timeval tv;
         gettimeofday(&tv, NULL);
-        //set _sys_acc_sec, _sys_acc_min
+        /* 获取当前时间的秒数，存储在 _sys_acc_sec 中 */
         _sys_acc_sec = tv.tv_sec;
+        /* 获取当前时间的分钟数，存储在 _sys_acc_min 中 */
         _sys_acc_min = _sys_acc_sec / 60;
-        //use _sys_acc_sec calc year, mon, day, hour, min, sec
         struct tm cur_tm;
+        /* 计算年、月、日、时、分和秒 */
         localtime_r((time_t*)&_sys_acc_sec, &cur_tm);
         year = cur_tm.tm_year + 1900;
         mon  = cur_tm.tm_mon + 1;
@@ -47,22 +48,24 @@ struct utc_timer
         reset_utc_fmt();
     }
 
+    // 获取当前时间的秒数，并更新年、月、日、时、分和秒
     uint64_t get_curr_time(int* p_msec = NULL)
     {
         struct timeval tv;
-        //get current ts
+        // 获取当前时间的秒数和微秒数
         gettimeofday(&tv, NULL);
         if (p_msec)
             *p_msec = tv.tv_usec / 1000;
-        //if not in same seconds
+        // 如果不在同一秒
         if ((uint32_t)tv.tv_sec != _sys_acc_sec)
         {
+            // 更新秒数和系统当前秒数
             sec = tv.tv_sec % 60;
             _sys_acc_sec = tv.tv_sec;
-            //or if not in same minutes
+            // 如果不在同一分钟
             if (_sys_acc_sec / 60 != _sys_acc_min)
             {
-                //use _sys_acc_sec update year, mon, day, hour, min, sec
+                
                 _sys_acc_min = _sys_acc_sec / 60;
                 struct tm cur_tm;
                 localtime_r((time_t*)&_sys_acc_sec, &cur_tm);
@@ -71,45 +74,55 @@ struct utc_timer
                 day  = cur_tm.tm_mday;
                 hour = cur_tm.tm_hour;
                 min  = cur_tm.tm_min;
-                //reformat utc format
+                // 重新格式化 UTC 时间
                 reset_utc_fmt();
             }
             else
             {
-                //reformat utc format only sec
+                // 仅更新秒数的 UTC 时间格式
                 reset_utc_fmt_sec();
             }
         }
         return tv.tv_sec;
     }
 
+    // 年、月、日、时、分、秒和 UTC 时间格式
     int year, mon, day, hour, min, sec;
     char utc_fmt[20];
 
 private:
+    // 格式化 UTC 时间
     void reset_utc_fmt()
     {
         snprintf(utc_fmt, 20, "%d-%02d-%02d %02d:%02d:%02d", year, mon, day, hour, min, sec);
     }
     
+    // 格式化 UTC 时间中的秒数
     void reset_utc_fmt_sec()
     {
         snprintf(utc_fmt + 17, 3, "%02d", sec);
     }
 
+    // 记录写上一条日志时，系统经过的时间
     uint64_t _sys_acc_min;
     uint64_t _sys_acc_sec;
 };
 
+
 class cell_buffer
 {
 public:
+    /* cell buffer的两个状态 */
     enum buffer_status
     {
-        FREE,
-        FULL
+        FREE,   /* 表示当前缓冲区还有空间 */
+        FULL    /* 表示暂时无法追加日志，正在、或即将被持久化到磁盘 */
     };
 
+    /**
+     * @brief cell_buffer初始化构造函数
+     * @param len 缓冲区长度（缓冲区只存放字符）
+     */
     cell_buffer(uint32_t len): 
     status(FREE), 
     prev(NULL), 
@@ -125,10 +138,21 @@ public:
         }
     }
 
+    /**
+     * @brief 返回当前缓冲区可用的大小
+     */
     uint32_t avail_len() const { return _total_len - _used_len; }
 
+
+    /**
+     * @brief 当前缓冲区是否为空
+     */
     bool empty() const { return _used_len == 0; }
 
+    /**
+     * @brief 向缓冲区中追加内容
+     * @param log_line 追加的字符串
+     */
     void append(const char* log_line, uint32_t len)
     {
         if (avail_len() < len)
@@ -137,12 +161,20 @@ public:
         _used_len += len;
     }
 
+    /**
+     * @brief 清空缓冲区
+     */
     void clear()
     {
+        /* 因为缓冲区的大小都是固定的，所以将_used_len置零即可 */
         _used_len = 0;
         status = FREE;
     }
 
+    /**
+     * @brief 持久化，从缓冲区中追加内容到文件(磁盘)
+     * @param fp 文件指针
+     */
     void persist(FILE* fp)
     {
         uint32_t wt_len = fwrite(_data, 1, _used_len, fp);
@@ -150,32 +182,41 @@ public:
         {
             fprintf(stderr, "write log to disk error, wt_len %u\n", wt_len);
         }
+        fflush(fp);
     }
 
     buffer_status status;
 
-    cell_buffer* prev;
-    cell_buffer* next;
+    cell_buffer* prev;  /* 指向前一个缓冲区 */
+    cell_buffer* next;  /* 指向后一个缓冲区 */
 
 private:
+    /* 下面两个函数，禁止对象拷贝、赋值 */
     cell_buffer(const cell_buffer&);
     cell_buffer& operator=(const cell_buffer&);
 
-    uint32_t _total_len;
-    uint32_t _used_len;
-    char* _data;
+    uint32_t _total_len;    /* 缓冲区长度 */
+    uint32_t _used_len;     /* 缓冲区已经使用的长度 */
+    char* _data;            /* 缓冲区 */
 };
 
 class ring_log
 {
 public:
-    //for thread-safe singleton
+    /**
+     * @brief 用于线程安全的单例模式
+     * @return 返回一个rain_log实例的指针
+     */
     static ring_log* ins()
     {
+        /* 本函数使用初值为PTHREAD_ONCE_INIT的once_control变量保证ring_log::init函数在本进程执行序列中仅执行一次。 */
         pthread_once(&_once, ring_log::init);
         return _ins;
     }
 
+    /**
+     * @brief 构建一个log实例
+     */
     static void init()
     {
         while (!_ins) _ins = new ring_log();
@@ -197,29 +238,30 @@ private:
     ring_log(const ring_log&);
     const ring_log& operator=(const ring_log&);
 
-    int _buff_cnt;
+    int _buff_cnt;              /* buffer的个数——缓存区链表的长度 */
 
-    cell_buffer* _curr_buf;
-    cell_buffer* _prst_buf;
+    cell_buffer* _curr_buf;     /* 生产者指针，指向当前缓存；该指针被多个生产者持有 */
+    cell_buffer* _prst_buf;     /* 消费者指针，指向持久化缓存；该指针只能被一个消费者持有 */
 
-    cell_buffer* last_buf;
+    cell_buffer* last_buf;      
 
-    FILE* _fp;
-    pid_t _pid;
-    int _year, _mon, _day, _log_cnt;
-    char _prog_name[128];
-    char _log_dir[512];
+    FILE* _fp;                  /* 需要写入日志的文件的指针 */     
+    pid_t _pid;                 /* 线程pid */ 
+    int _year, _mon, _day;      
+    int _log_cnt;               /* 日志文件计数器，从0开始 */  
+    char _prog_name[128];       /* 程序名称 */
+    char _log_dir[512];         
 
-    bool _env_ok;//if log dir ok
-    int _level;
-    uint64_t _lst_lts;//last can't log error time(s) if value != 0, log error happened last time
+    bool _env_ok;               /* 当前日志的文件夹是否正常 */
+    int _level;                 /* 日志级别 */
+    uint64_t _lst_lts;          /* 代表上一次日志写入的时间戳(单位s)，如果该值非0，最后不能记录错误时间，记录错误发生在上次 */
     
     utc_timer _tm;
 
     static pthread_mutex_t _mutex;
     static pthread_cond_t _cond;
 
-    static uint32_t _one_buff_len;
+    static uint32_t _one_buff_len;  /* 一个缓冲区的大小 */
 
     //singleton
     static ring_log* _ins;
@@ -386,4 +428,4 @@ void* be_thdo(void* args);
             gettid(), __FILE__, __LINE__, __FUNCTION__, ##args); \
     } while (0)
 
-#endif //LOG_H__
+#endif
